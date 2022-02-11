659112c9bd10adeb17380e12615db95e2c317af0
Muhammad Sabtain <sabtain40@gmail.com>
2022-02-11 11:48:05 +0500

    ex 4 added in var_ex_1 file

--- var_ex_1.rb
+++ var_ex_1.rb
@@ -7,5 +7,12 @@ print "Enter your name please: "
 name = gets.chomp
 puts "Welcome #{name}"
 # ex 3
-10.times { print name + ' '}
+10.times { puts  name + ' '}
+
+#ex 4
+print "Enter your first name: "
+first_name = gets.chomp
+print "Enter your last name: "
+last_name = gets.chomp
+puts first_name + " " + last_name
 

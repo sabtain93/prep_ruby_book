cf74aa491eb17a0e2d6f0364dddcfd0de143483f
Muhammad Sabtain <sabtain40@gmail.com>
2022-02-11 11:37:20 +0500

    adding ex # 2

---
+++ age.rb
@@ -0,0 +1,18 @@
+# variable excersie 2
+# I/P age from user print out the age in 10,20,30,40 years
+
+print "Enter your current age here: "
+
+age = Integer(gets.chomp)
+
+puts "In 10 years you will be:"
+puts age + 10
+puts "In 20 years you will be:"
+puts age + 20
+puts "In 30 years you will be:"
+puts age + 30
+puts "In 40 years you will be:"
+puts age + 40
+
+
+

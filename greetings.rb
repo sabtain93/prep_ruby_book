651638e7f47cc8c3480ab85e2f882a94989f31aa
Muhammad Sabtain <sabtain40@gmail.com>
2022-02-16 10:57:31 +0500

    adding method section excersies

---
+++ greetings.rb
@@ -0,0 +1,10 @@
+# method section excersie 1
+
+def greeting(name)
+   "Hi #{name}, welcome to ruby"
+end
+
+puts greeting("sabtain")
+
+
+
---
+++ multiply.rb
@@ -0,0 +1,7 @@
+#method exercise 3
+def multiply(a, b)
+  a * b
+end
+
+multiply(2, 3)
+

fbb664be33b2f4a7e08814f4dfb07170a49ea37d
Muhammad Sabtain <sabtain40@gmail.com>
2022-03-22 10:26:54 +0500

    array ex # 1

---
+++ arr_1.rb
@@ -0,0 +1,14 @@
+# array excersie # 1
+
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+a = arr.include?(number)
+
+if a == true
+  puts "#{number} is in arr"
+else
+  puts "#{number} is not in arr"
+end
+

c7811aa75dd5faf950e6156a8f5fa8ce2a3930a1
Muhammad Sabtain <sabtain40@gmail.com>
2022-03-22 11:10:10 +0500

    adding array ex # 7

---
+++ arr_3.rb
@@ -0,0 +1,7 @@
+
+# array exercise # 3
+
+arr = [["test", "hello", "world"],["example", "mem"]]
+
+arr.select{ |x| x == "example"}
+
---
+++ arr_7.rb
@@ -0,0 +1,5 @@
+# array excersie # 7
+arr = ["ibrahim", "yusuf", "badar", "khadija", "fatima"]
+
+arr.each_with_index{|val, indx| puts "#{indx}. #{val}"}
+

d3c6cc4f0a590d9ae1df6696a4e57a504bc9b92d
Muhammad Sabtain <sabtain40@gmail.com>
2022-03-30 10:52:30 +0500

    hash exercise # 5

---
+++ hash_ex_5.rb
@@ -0,0 +1,13 @@
+# hash exercise # 5
+
+my_hash = {name: 'sabtain', age: 28, hair: 'long'}
+
+answer = my_hash.value?('sabtain')
+
+if answer == true
+  puts " the value exist"
+else
+ puts " the value does not exist"
+end
+
+

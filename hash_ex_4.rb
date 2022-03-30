79b71383d2a8732868a34cf2dc7eefedcde5d30f
Muhammad Sabtain <sabtain40@gmail.com>
2022-03-30 10:45:24 +0500

    hash exercise # 4

---
+++ hash_ex_4.rb
@@ -0,0 +1,14 @@
+# hash exercise # 4
+
+
+
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+
+# name =  person.select{|k, v| k == :name}
+
+# name_1 = name.values
+
+#puts name_1
+
+person[:name]
+
